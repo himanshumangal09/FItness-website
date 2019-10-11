7b954f2d86662a59d894564bcf33e8998d9026fd
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 20:23:46 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -29,7 +29,7 @@ class PlanShow extends React.Component {
 		}
 		return (
 			<div className='ui form'>
-				<UserDetails/tails>
+				<UserDetails/ails>
 				<FoodDetails data={this.state.inputvalueforfooddetails} />
 			</div>
 		);
