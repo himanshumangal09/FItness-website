79f0b86b242ac13705bcc28bd30d1186c1d1ac71
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 18:42:21 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -99,7 +99,7 @@ class PlanShow extends React.Component {
 	// 				}
 	 				if (
 	// 					(this.state.Sex == "m" && this.state.waist >= 40) ||
-	 					(this.state.Sex == "f" && this.state.waist >= 35)
+						(this.state.Sex == "f" && this.state.waist >= 35)
 	/ 				) {
 	 					this.setState({ diseaseRisk: "very high" });
 	 				}
