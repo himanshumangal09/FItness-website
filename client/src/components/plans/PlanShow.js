1356b047de9b44c330ddfab270037aa7cefdb3ac
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 22:24:17 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -7,21 +7,7 @@ import UserDetails from "./UserDetails";
 const bodyMassIndex = require("body-mass-index");
 const axios = require("axios");
 class PlanShow extends React.Component {
-	// state = {
-	// 	inputfoodvalue: null,
-	// 	inputvalueforfooddetails: null,
-	// 	bmi: 0,
-	// 	Age: 0,
-	// 	Weight: 0,
-	// 	Height: 0,
-	// 	Name: 0,
-	// 	Sex: 0,
-	// 	Plan  : 0,
-	// 	waist: 0,
-	// 	hip: 0,
-	// 	typeOfPerson: 0 ,
-	// 	diseaseRisk: 0
-	// };
+
 	componentDidMount() {
 		this.props.fetchplan(this.props.match.params.id);
 	
