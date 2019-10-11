cb022f9d536e65e2dcc42da9ab8a152e55a9c33f
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 22:34:10 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -10,7 +10,6 @@ class PlanShow extends React.Component {
 	};
 	componentDidMount() {
 		this.props.fetchplan(this.props.match.params.id);
-
 	}
 	helpplease = () => {
 		if (document.getElementById("inputvalueforfooddetails").value != null) {
