9c0e5872f4c711efa11305921603208834cb1d94
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 20:23:23 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -6,8 +6,7 @@ import FoodDetails from "./FoodDetailsWhenCalled";
 import UserDetails from './UserDetails';
 class PlanShow extends React.Component {
 	componentDidMount() {
-		this.props.fetchplan(this.props.match.params.id);
-	}
+
 	helpplease = () => {
 		if (document.getElementById("inputvalueforfooddetails").value != null) {
 			this.setState({
