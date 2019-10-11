315d0f202e34bab9b88874a61b41e8cfc5b4822f
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 22:26:24 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -37,7 +37,7 @@ class PlanShow extends React.Component {
 		return (			
 			<div>
 			<UserDetails data={this.props.plan}/>
-			<FitnessVideos/FitnessVideos>
+			<FitnessVideos/itnessVideos>
 			</div>
 		);
 	}
