719a2d58067c61cd2a098bcc5aebe4cacc3f4c7c
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 20:22:37 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { fetchplan } from "../../actions/index";
 import FitnessVideos from "./FitnessVideos";
 import FoodDetails from "./FoodDetailsWhenCalled";
-
+i
 class PlanShow extends React.Component {
 	state = {
 		inputfoodvalue: null,
