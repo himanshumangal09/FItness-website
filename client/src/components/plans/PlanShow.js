e7dd63a074de29f9129daaba1715cf16bae02e84
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 18:41:14 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -135,7 +135,7 @@ class PlanShow extends React.Component {
 	// 				</div>
 	// 				<div className='ui green segment'>
 	// 					<p>Weight(in KG):{this.state.Weight}</p>
-	/ 				</div>
+	 				</div>
 	/ 				<div className='ui yellow segment'>
 	 					<p> Height(in cm):{this.state.Height}</p>
 	 				</div>
