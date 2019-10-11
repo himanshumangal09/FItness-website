171c8f29c66b1da6a825ce66123f119a71ac5b64
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 18:42:25 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -96,7 +96,7 @@ class PlanShow extends React.Component {
 	// 					(this.state.Sex == "f" && this.state.waist <= 35)
 	 				) {
 	/ 					this.setState({ diseaseRisk: "very High" });
-	/ 				}
+	 				}
 					if (
 	 					(this.state.Sex == "m" && this.state.waist >= 40) ||
 						(this.state.Sex == "f" && this.state.waist >= 35)
