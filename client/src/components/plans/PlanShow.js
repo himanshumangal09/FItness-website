ee1d84e31d0c155eeb33e7aa4c47cd657d80fdec
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 18:41:09 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -141,7 +141,7 @@ class PlanShow extends React.Component {
 	// 				</div>
 	// 				<div className='ui yellow segment'>
 	 					<p> Waist(in cm): {this.state.waist}</p>
-	/ 				</div>
+	 				</div>
 	 				<div className='ui yellow segment'>
 						<p>Hip(in cm):{this.state.hip}</p>
 	 				</div>
