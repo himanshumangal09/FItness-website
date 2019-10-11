73bac9172a9ab08c3bfd1820380389fd795927d1
9313506071 <himanshu9555548291@gmail.com>
2019-10-11 18:40:43 +0530

    autocommit on change

--- client/src/components/plans/PlanShow.js
+++ client/src/components/plans/PlanShow.js
@@ -157,7 +157,7 @@ class PlanShow extends React.Component {
 	// 				<div className='ui blue segment'>
 	// 					<p>
  						Disease Risk (like type 2 diabetes,hypertension,CVD):{" "}
-	 						{this.state.diseaseRisk}
+							{this.state.diseaseRisk}
 	 					</p>
 			</div>
  			</div>
