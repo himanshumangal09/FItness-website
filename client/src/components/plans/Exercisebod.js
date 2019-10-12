a99870999ebc3c9283c47c7569dfbb1c24de23a9
9313506071 <himanshu9555548291@gmail.com>
2019-10-12 10:37:38 +0530

    autocommit on change

--- client/src/components/plans/Exercisebod.js
+++ client/src/components/plans/Exercisebod.js
@@ -1,7 +1,7 @@
 import React from "react";
 class Exercisebod extends React.Component {
     exerciseBasedOnDetails() {
-        const {typeOfPerson,Age}=this
+        const {typeOfPerson,Age}=this.prop
         if(this.props.typeOfPerson!=undefined&&this.props.Age!=undefined)
         {
             if(typeOfPerson="Underweight" && (Age>15||Age<22))
