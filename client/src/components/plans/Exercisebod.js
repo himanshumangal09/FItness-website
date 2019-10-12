6923115e8d9d04f2ba6d65e8539e7b13ced6f1b2
9313506071 <himanshu9555548291@gmail.com>
2019-10-12 10:44:37 +0530

    autocommit on change

--- client/src/components/plans/Exercisebod.js
+++ client/src/components/plans/Exercisebod.js
@@ -21,7 +21,7 @@ class Exercisebod extends React.Component {
 					</div>
 				);
 			}
-			if (typeOfPerson == "Underweight" && (Age > 22 |Age < 30)) {
+			if (typeOfPerson == "Underweight" && (Age >Age < 30)) {
 				return (
 					<div>
 						<div>exercise 7</div>
