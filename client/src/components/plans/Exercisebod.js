6d294cce4fd7ed0d19f74f151884340a59f2c594
9313506071 <himanshu9555548291@gmail.com>
2019-10-12 10:38:55 +0530

    autocommit on change

--- client/src/components/plans/Exercisebod.js
+++ client/src/components/plans/Exercisebod.js
@@ -13,7 +13,9 @@ class Exercisebod extends React.Component {
                                 <div>
                                 exercise 1
                            </div>
-                           
+                                           <div>
+                                           exercise 1
+                                      </div>
                 )   
             }
         }
