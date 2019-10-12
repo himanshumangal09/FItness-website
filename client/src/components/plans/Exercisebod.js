170f575bccc8e6c8a0ccc88356e8d2a56c3a13d4
9313506071 <himanshu9555548291@gmail.com>
2019-10-12 10:39:36 +0530

    autocommit on change

--- client/src/components/plans/Exercisebod.js
+++ client/src/components/plans/Exercisebod.js
@@ -1,34 +1,21 @@
 import React from "react";
 class Exercisebod extends React.Component {
-    exerciseBasedOnDetails() {
-        const {typeOfPerson,Age}=this.props;
-        if(typeOfPerson!=undefined&&Age!=undefined)
-        {
-            if(typeOfPerson="Underweight" && (Age>15||Age<22))
-            {
-                 return(
-                     <div>
-                <div>
-                     exercise 1
-                </div>
-                                <div>
-                                exercise 2
-                           </div>
-                                           <div>
-                                           exercise 3
-                                      </div>
-                     </div>
+	exerciseBasedOnDetails() {
+		const { typeOfPerson, Age } = this.props;
+		if (typeOfPerson != undefined && Age != undefined) {
+			if ((typeOfPerson = "Underweight" && (Age > 15 || Age < 22))) {
+				return (
+					<div>
+						<div>exercise 1</div>
+						<div>exercise 2</div>
+						<div>exercise 3</div>
+					</div>
+				);
+			}
+		}
+	}
 
-                )   
-            }
-        }
-    }
-    
-    
-    
-    
-    
-    render() {
+	render() {
 		console.log(this.props);
 		return <div>Exercisebod</div>;
 	}
